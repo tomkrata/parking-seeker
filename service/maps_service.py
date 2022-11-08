51c0728b2c2c7c362895e9f0da0aba32c2af8d05
kratot13 <kratot13@cvut.cz>
2022-11-08 15:45:12 +0100

    service to get lat lon from place

---
+++ service/maps_service.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+
+def getLatLon(search_query):
+    search_query = search_query.replace(' ', '%20')
+    url = f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" \
+          f"{search_query}" \
+          f"&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=" \
+          f"AIzaSyAxqtpGpVxUcIzrsHjYL07IgIyijBPE_8Q"
+
+    payload = {}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    obj = json.loads(response.text)
+    if obj['status'] != 'OK' or len(obj['candidates']) == 0:
+        return None, None
+    addr = obj['candidates'][0]
+    return addr['formatted_address'], [val for (key, val) in addr['geometry']['location'].items()]
+
+
+if __name__ == "__main__":
+    place, coords = getLatLon('Ferrariho 8 Praha')
+    print(f'place: "{place}" is located at {coords[0]},{coords[1]}')
