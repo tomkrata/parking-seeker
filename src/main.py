69cbd45e465ad42d1181a683d701109cd332ab06
kratot13 <kratot13@cvut.cz>
2023-01-05 13:51:39 +0100

    first commit

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restx import Api
 from pymongo import MongoClient
@@ -29,5 +31,6 @@ if __name__ == "__main__":
     # app.register_blueprint(main_blueprint)
 
     """Run Flask app"""
-    flask_app.run(debug=True)
+    # flask_app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
